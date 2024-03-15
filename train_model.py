22abcc2b1e1c0c6b52e3eafc69a1e86343dead09
Zhuiri Xiao <3356142450@qq.com>
2024-03-16 00:48:50 +0800

    add train record-1

--- train_model.py
+++ train_model.py
@@ -6,5 +6,5 @@ from ultralytics import YOLO
 #load a pre-trained model
 model = YOLO('./weights/yolov8s.pt')  # build from YAML and transfer weights
 # Train the model
-model.train(data='./robo.yaml', epochs=100, imgsz=1536,batch=8, workers=0,device="cuda")
+model.train(data='./robo.yaml', epochs=300, imgsz=1536,batch=16, workers=8,device="cuda")
 # model.export(format='')
